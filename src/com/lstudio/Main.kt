113872570fa46b7dc53a049e2a3c64e000c378a7
LStudio <v.storchevyi@gmail.com>
2019-04-04 11:02:59 +0300

    Add manual distance resolver

--- src/com/lstudio/Main.kt
+++ src/com/lstudio/Main.kt
@@ -30,7 +30,7 @@ object Main {
         val endDepots = taskReader.endDepots ?: return
         val startDepots = taskReader.startDepots ?: return
 
-        val decision = 4
+        val decision = 5
         when (decision) {
             1 -> {
                 val antColony = AntColonyOptimization(weight, startDepots, endDepots)
@@ -49,8 +49,42 @@ object Main {
                 val bruteforceSolver = BruteforceSolver(startDepots, endDepots, weight)
                 bruteforceSolver.solve()
             }
+            5 -> {
+
+                /**
+                 *Distance between first and last and consecutive elements of a list.
+                 **/
+                fun distance(x: Int, y: Int): Double {
+                    return weight[x][y]
+                }
+
+                /**
+                 * Distance between first and last and consecutive elements of a list
+                 */
+                fun routeLength(route: List<Int>): Double {
+                    var sum = 0.0
+                    for (i in 1 until route.size) sum += distance(route[i], route[i - 1])
+                    sum += distance(route[0], route[route.size - 1])
+                    return sum
+                }
+
+                println("Enter solution")
+                //val line = readLine()
+                //val line1 = "[0, 2, 6, 10, 11, 12, 13, 9, 7, 5, 15]-[1, 3, 14]-[4, 8, 14]"
+                val line1 = "[0, 2, 5, 7, 9, 13, 12, 15]-[1, 3, 11, 14]-[4, 6, 8, 10, 14]"
+                val group = line1.split("-")
+                val newGroup = group.map { it.drop(1).dropLast(1) }.map { it.split(", ") }
+                var sum = 0.0
+                newGroup.forEach{
+                    val result = it.map { it.toInt() }
+                    sum += routeLength(result)
+                }
+                println("Sum: $sum")
+            }
             else -> println("Unknown option")
         }
         scanner.close()
     }
+
+
 }
